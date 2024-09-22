db1508e8480c7d657feaac572d3a4840fac5e446
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-09-22 21:06:15 +0530

    db intialized and config id logic

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,8 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from 'zod';
 //shift + alt + o => remove unused imports
+import sharp from 'sharp'
+import { db } from "@/app/db";
 
 const f = createUploadthing();
 
@@ -13,9 +15,44 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
 
-        const { configId } = metadata.input
- 
-      return { configId };
+      
+        const { configId } = metadata.input //something that you get from frontend
+
+        console.log('file after upload>>>>>>>', file);
+        
+
+        const res = await fetch(file.url)
+
+        console.log('res from the fetch, idk why is it being done>>>>>', res);
+
+        const buffer = await res.arrayBuffer()
+        
+        const imgMetadata = await sharp(buffer).metadata()
+       
+        const { width, height, } = imgMetadata  
+
+        if (!configId) {
+              const configuration = await db.configuration.create({
+                data: {
+                  imageUrl: file.url,
+                  height: height || 500,
+                  width: width || 500,
+                },
+              })
+
+              return { configId: configuration.id }
+            } else {
+                  const updatedConfiguration = await db.configuration.update({
+                    where: {
+                      id: configId,
+                    },
+                    data: {
+                      croppedImageUrl: file.url,
+                    },
+                  })
+          
+                  return { configId: updatedConfiguration.id }
+                }
     }),
 } satisfies FileRouter;
  

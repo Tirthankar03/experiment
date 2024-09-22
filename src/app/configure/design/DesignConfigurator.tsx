e45f20c548b135f35367591f14946c290fb45191
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-09-23 02:03:40 +0530

    dynamic color switch using radio group added
    headless ui used for radio group

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,9 +1,17 @@
-'use client'
+"use client";
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-950 border-rose-950
 import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import NextImage from 'next/image'
-import { Rnd } from 'react-rnd'
+import NextImage from "next/image";
+import { Rnd } from "react-rnd";
+import { RadioGroup } from '@headlessui/react'
+import { COLORS } from "@/validators/option-validator";
+import { useState } from "react";
+import { Label } from "@/components/ui/label";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -11,48 +19,58 @@ interface DesignConfiguratorProps {
   imageDimensions: { width: number; height: number };
 }
 
+
+
 const DesignConfigurator = ({
   configId,
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+
+
+
+  const [options, setOptions] = useState<{
+    color: (typeof COLORS)[number]
+  }>({
+    color: COLORS[0],
+  })
+
+
   return (
     <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
-        
         {/* this div ensures that the aspect ration is maintained */}
-        <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
-        {/* z-50 given to make the image border pop up above the shadow */}
-        <AspectRatio
+        <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
+          {/* z-50 given to make the image border pop up above the shadow */}
+          <AspectRatio
             ratio={896 / 1831}
-            className='pointer-events-none relative z-50 aspect-[896/1831] w-full'>
+            className="pointer-events-none relative z-50 aspect-[896/1831] w-full"
+          >
             <NextImage
               fill
-              alt='phone image'
-              src='/phone-template.png'
-              className='pointer-events-none z-50 select-none'
+              alt="phone image"
+              src="/phone-template.png"
+              className="pointer-events-none z-50 select-none"
             />
           </AspectRatio>
           {/* outside */}
           {/* a shadow that has a large spread, convering the entire container with z-40 */}
-          <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
+          <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
           {/* inside */}
 
           {/* hard coded absolute color background positioned such that it matches with transparent phone img */}
           {/* no height given, aspect ratio defined in parent component */}
           <div
             className={cn(
-              'absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]',
-              // `bg-${options.color.tw}`
-              `bg-blue-950`
+              "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
+              `bg-${options.color.tw}`
             )}
           />
         </div>
 
-
-            {/* the draggable logic */}
-            {/* use rnd: draggable library */}
-            <Rnd
+        {/* the draggable logic */}
+        {/* use rnd: draggable library */}
+        <Rnd
           default={{
             x: 150,
             y: 205,
@@ -66,17 +84,78 @@ const DesignConfigurator = ({
             topRight: <HandleComponent />,
             topLeft: <HandleComponent />,
           }}
-          className='absolute z-20 border-[3px] border-primary'  
-          >
-            <div className='relative w-full h-full'>
+          className="absolute z-20 border-[3px] border-primary"
+        >
+          <div className="relative w-full h-full">
             <NextImage
               src={imageUrl}
               fill
-              alt='your image'
-              className='pointer-events-none'
+              alt="your image"
+              className="pointer-events-none"
             />
           </div>
-          </Rnd>
+        </Rnd>
+      </div>
+      <div className="h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          {/* adds a gradient background */}
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+            {/* seperator */}
+            <div className='w-full h-px bg-zinc-200 my-6' />
+
+
+            <div className='relative mt-4 h-full flex flex-col justify-between'>
+            <div className='flex flex-col gap-6'>
+            <RadioGroup
+                  value={options.color}
+                  onChange={(val) => {
+                    // we just want to change only the color attribute from the entire state
+                    setOptions((prev) => ({
+                      ...prev,
+                      color: val,
+                    }))
+                  }}>
+                  <Label>Color: {options.color.label}</Label>
+                  <div className='mt-3 flex items-center space-x-3'>
+                  {COLORS.map((color) => (
+                      <RadioGroup.Option
+                        key={color.label}
+                        value={color}
+                        className={({ active, checked }) =>
+                          cn(
+                            'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent',
+                            {
+                              [`border-${color.tw}`]: active || checked,
+                            }
+                          )
+                        }>
+                        <span
+                          className={cn(
+                            `bg-${color.tw}`,
+                            'h-8 w-8 rounded-full border border-black border-opacity-10'
+                          )}
+                        />
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+
+                  </RadioGroup>
+            </div>
+            </div>
+          
+          
+          </div>
+
+
+        </ScrollArea>
       </div>
     </div>
   );

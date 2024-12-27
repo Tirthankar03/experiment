cd2b598c9b9a634985e3d3645e5283cb81a61418
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-12-27 21:56:26 +0530

    experiment 1: css grids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-
-import { auth } from "@/auth";
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
@@ -9,48 +7,190 @@ import { buttonVariants } from "@/components/ui/button";
 import { ArrowRight, Check, Star } from "lucide-react";
 import Link from "next/link";
 
-
-export default async function Home() {
-  const session = await auth()
-
-
-  console.log({session});
-  
+export default function Home() {
   return (
     <div className="bg-slate-50 grainy-light">
+      {/* Defining a Grid Container*/}
+      <section>
+        <div className="my-10">
+          <div className="grid grid-cols-3 gap-4">
+            <div className="bg-blue-200 p-4">1</div>
+            <div className="bg-blue-300 p-4">2</div>
+            <div className="bg-blue-400 p-4">3</div>
+          </div>
+        </div>
+
+        {/* Fixed Column Sizes:*/}
+        <div className="my-10">
+          <div className="grid grid-cols-4 gap-2">
+            <div className="bg-red-200 p-2">1</div>
+            <div className="bg-red-300 p-2">2</div>
+            <div className="bg-red-400 p-2">3</div>
+            <div className="bg-red-500 p-2">4</div>
+          </div>
+        </div>
+
+        {/* Variable Column Sizes: */}
+        <div className="my-10">
+          <div className="grid grid-cols-[100px,200px,1fr] gap-2">
+            <div className="bg-green-200 p-2">100px</div>
+            <div className="bg-green-300 p-2">200px</div>
+            <div className="bg-green-400 p-2">Flexible</div>
+          </div>
+        </div>
+
+        {/* Defining Rows: */}
+        <div className="my-10">
+          <div className="grid grid-rows-3 gap-4">
+            <div className="bg-yellow-200 p-2">Row 1</div>
+            <div className="bg-yellow-300 p-2">Row 2</div>
+            <div className="bg-yellow-400 p-2">Row 3</div>
+          </div>
+        </div>
+
+        <div className="my-10">
+          <div className="grid grid-cols-4 gap-2">
+            <div className="col-span-2 bg-indigo-200 p-2">Spans 2 columns</div>
+            <div className="col-span-1 bg-indigo-300 p-2">Spans 1 column</div>
+            <div className="col-span-1 bg-indigo-400 p-2">Spans 1 column</div>
+            <div className="row-span-2 bg-indigo-500 p-2">Spans 2 rows</div>
+          </div>
+        </div>
+
+        <div className="my-10">
+          <div className="grid grid-cols-3 justify-items-center gap-2">
+            <div className="bg-pink-200 p-2">Center 1</div>
+            <div className="bg-pink-300 p-2">Center 2</div>
+            <div className="bg-pink-400 p-2">Center 3</div>
+          </div>
+        </div>
+        <div className="my-10">
+          <div className="grid grid-rows-3 items-center gap-2">
+            <div className="bg-teal-200 p-2">Center Row 1</div>
+            <div className="bg-teal-300 p-2">Center Row 2</div>
+            <div className="bg-teal-400 p-2">Center Row 3</div>
+          </div>
+        </div>
+
+        <div className="my-10">
+            <div className="grid grid-cols-3 gap-x-8 gap-y-4">
+            <div className="bg-gray-200 p-2">1</div>
+            <div className="bg-gray-300 p-2">2</div>
+            <div className="bg-gray-400 p-2">3</div>
+            <div className="bg-gray-500 p-2">4</div>
+            <div className="bg-gray-600 p-2">5</div>
+          </div>
+        </div>
+
+        <div className="my-10">
+          <div className="grid grid-cols-3 gap-x-8 gap-y-4">
+            <div className="bg-gray-200 p-2">1</div>
+            <div className="bg-gray-300 p-2">2</div>
+            <div className="bg-gray-400 p-2">3</div>
+            <div className="bg-gray-500 p-2">4</div>
+            <div className="bg-gray-600 p-2">5</div>
+          </div>
+        </div>
+
+        <div className="my-10">
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-4">
+            <div className="bg-orange-200 p-2">1</div>
+            <div className="bg-orange-300 p-2">2</div>
+            <div className="bg-orange-400 p-2">3</div>
+          </div>
+        </div>
+
+        <div className="my-10">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="bg-purple-200 p-4">
+              <div className="grid grid-cols-2 gap-2">
+                <div className="bg-purple-300 p-2">Nested 1</div>
+                <div className="bg-purple-400 p-2">Nested 2</div>
+              </div>
+            </div>
+            <div className="bg-purple-500 p-4">Parent 2</div>
+          </div>
+        </div>
+
+        <div className="my-10">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="bg-cyan-200 p-2">1</div>
+            <div className="bg-cyan-300 p-2">2</div>
+            <div className="bg-cyan-400 p-2">3</div>
+              <div className="bg-cyan-500 p-2">4</div>
+            </div>
+        </div>
+
+      <div className="my-10">
+        <div className="grid gap-4"
+            style={{
+              gridTemplateAreas: `
+              "header header"
+              "sidebar content"
+              "footer footer"
+              `,
+              gridTemplateColumns: "1fr 3fr",
+              gridTemplateRows: "auto 1fr auto",
+    }}
+          >
+            <div className="bg-blue-200 p-4" style={{ gridArea: "header" }}>Header</div>
+            <div className="bg-blue-300 p-4" style={{ gridArea: "sidebar" }}>Sidebar</div>
+            <div className="bg-blue-400 p-4" style={{ gridArea: "content" }}>Content</div>
+            <div className="bg-blue-500 p-4" style={{ gridArea: "footer" }}>Footer</div>
+        </div>
+      </div>
+
+      <div className="my-10">
+        <div className="grid min-h-[1000px] grid-cols-[200px,1fr,1fr] grid-rows-[70px,1fr,70px] gap-4" 
+          style={{
+            gridTemplateAreas: `
+              "header header header"
+              "nav main main"
+              "footer footer footer"
+            `,
+          }}
+        >
+          <div className="bg-red-200 p-4" style={{ gridArea: "header" }}>Header</div>
+          <div className="bg-red-300 p-4" style={{ gridArea: "nav" }}>nav</div>
+          <div className="bg-red-400 p-4" style={{ gridArea: "main" }}>main</div>
+          <div className="bg-red-500 p-4" style={{ gridArea: "footer" }}>footer</div>
+        </div>
+      </div>
+      </section>
+
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-              <div className="absolute w-40 left-0 -top-20 hidden lg:block">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                 {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
-                <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t" />
-                <img src="/phone-step1-removebg.png" className="w-full" />
+                <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28" />
+                <img src="/snake-1.png" className="w-full" />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your Image on a{" "}
-                <span className="bg-rose-600 px-2 text-white">Custom</span>{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
                 Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favorite memories with your own,{" "}
                 <span className="font-semibold">one-of-one</span> phone case.
-                CupCase allows you to protect your memories, not just your
+                CaseCobra allows you to protect your memories, not just your
                 phone case.
               </p>
 
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                 <div className="space-y-2">
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-rose-600" />
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
                     High-quality, durable material
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-rose-600" />5 year
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
                     print guarantee
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-rose-600" />
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
                     Modern iPhone models supported
                   </li>
                 </div>
@@ -87,15 +227,15 @@ export default async function Home() {
 
                 <div className="flex flex-col justify-between items-center sm:items-start">
                   <div className="flex gap-0.5">
-                    <Star className="h-4 w-4 text-rose-600 fill-rose-600" />
-                    <Star className="h-4 w-4 text-rose-600 fill-rose-600" />
-                    <Star className="h-4 w-4 text-rose-600 fill-rose-600" />
-                    <Star className="h-4 w-4 text-rose-600 fill-rose-600" />
-                    <Star className="h-4 w-4 text-rose-600 fill-rose-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
                   </div>
 
                   <p>
-                    <span className="font-semibold">1,250</span> happy customers
+                    <span className="font-semibold">1.250</span> happy customers
                   </p>
                 </div>
               </div>
@@ -118,35 +258,29 @@ export default async function Home() {
         </MaxWidthWrapper>
       </section>
 
-
-
-
-
-
-
-            {/* value proposition section */}
-            <section className="bg-slate-100 grainy-dark py-24">
+      {/* value proposition section */}
+      <section className="bg-slate-100 grainy-dark py-24">
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
               What our{" "}
               <span className="relative px-2">
                 customers{" "}
-                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-rose-500" />
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
               </span>{" "}
               say
             </h2>
-            <img src="/phone-step2-removebg.png" className="w-40 order-0 lg:order-2" />
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
           </div>
 
           <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-0.5 mb-2">
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
               </div>
               <div className="text-lg leading-8">
                 <p>
@@ -168,7 +302,7 @@ export default async function Home() {
                 <div className="flex flex-col">
                   <p className="font-semibold">Jonathan</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
-                    <Check className="h-4 w-4 stroke-[3px] text-rose-600" />
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
                     <p className="text-sm">Verified Purchase</p>
                   </div>
                 </div>
@@ -178,11 +312,11 @@ export default async function Home() {
             {/* second user review */}
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-0.5 mb-2">
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
-                <Star className="h-5 w-5 text-rose-600 fill-rose-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
               </div>
               <div className="text-lg leading-8">
                 <p>
@@ -205,7 +339,7 @@ export default async function Home() {
                 <div className="flex flex-col">
                   <p className="font-semibold">Josh</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
-                    <Check className="h-4 w-4 stroke-[3px] text-rose-600" />
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
                     <p className="text-sm">Verified Purchase</p>
                   </div>
                 </div>
@@ -214,13 +348,10 @@ export default async function Home() {
           </div>
         </MaxWidthWrapper>
 
-
-
         <div className="pt-16">
           <Reviews />
         </div>
       </section>
-
 
       <section>
         <MaxWidthWrapper className="py-24">
@@ -228,7 +359,7 @@ export default async function Home() {
             <div className="mx-auto max-w-2xl sm:text-center">
               <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
                 Upload your photo and get{" "}
-                <span className="relative px-2 bg-rose-600 text-white">
+                <span className="relative px-2 bg-green-600 text-white">
                   your own case
                 </span>{" "}
                 now
@@ -256,19 +387,19 @@ export default async function Home() {
 
           <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
             <li className="w-fit">
-              <Check className="h-5 w-5 text-rose-600 inline mr-1.5" />
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
               High-quality silicone material
             </li>
             <li className="w-fit">
-              <Check className="h-5 w-5 text-rose-600 inline mr-1.5" />
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
               Scratch- and fingerprint resistant coating
             </li>
             <li className="w-fit">
-              <Check className="h-5 w-5 text-rose-600 inline mr-1.5" />
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
               Wireless charging compatible
             </li>
             <li className="w-fit">
-              <Check className="h-5 w-5 text-rose-600 inline mr-1.5" />5 year
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />5 year
               print warranty
             </li>
 
@@ -285,8 +416,7 @@ export default async function Home() {
             </div>
           </ul>
         </MaxWidthWrapper>
-
       </section>
-      </div>
+    </div>
   );
 }
